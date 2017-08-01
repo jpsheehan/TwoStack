77aaf0d3e93ac20d3c231bfdc4b04cce5ef45876
Jesse Sheehan <jesse@sheehan.nz>
2017-08-01 15:10:55 +1200

    Added a debug method

--- ulang.py
+++ ulang.py
@@ -24,6 +24,251 @@ class UcLang(object):
 
   def __init__(self):
     self.reset()
+
+    commands = {
+      '"': {
+        'name': 'string literal',
+        'min': 0,
+        'function': self.op_stringliteral
+      },
+      '\n': {
+        'name': 'newline',
+        'min': 0,
+        'function': self.op_newline
+      },
+      ' ': {
+        'name': 'whitespace',
+        'min': 0,
+        'function': self.op_whitespace
+      },
+      ';': {
+        'name': 'discard',
+        'min': 1,
+        'function': self.op_discard
+      },
+      ':': {
+        'name': 'duplicate',
+        'min': 1,
+        'function': self.op_duplicate
+      },
+      '\\': {
+        'name': 'swap',
+        'min': 2,
+        'function': self.op_swap
+      },
+      '\\\\': {
+        'name': '3-swap',
+        'min': 3,
+        'function': self.op_3swap
+      },
+      '+': {
+        'name': 'add',
+        'min': 2,
+        'function': self.op_add
+      },
+      '-': {
+        'name': 'subtract',
+        'min': 2,
+        'function': self.op_subtract
+      },
+      '*': {
+        'name': 'multiply',
+        'min': 2,
+        'function': self.op_multiply
+      },
+      '/': {
+        'name': 'divide',
+        'min': 2,
+        'function': self.op_divide
+      },
+      '//': {
+        'name': 'int divide',
+        'min': 2,
+        'function': self.op_intdivide
+      },
+      '%': {
+        'name': 'modulo',
+        'min': 2,
+        'function': self.op_modulo
+      },
+      '^': {
+        'name': 'power',
+        'min': 2,
+        'function': self.op_power
+      },
+      '.': {
+        'name': 'print',
+        'min': 1,
+        'function': self.op_print
+      },
+      '`': {
+        'name': 'crosspop',
+        'min': 1,
+        'function': self.op_crosspop
+      },
+      '$': {
+        'name': 'stackswap',
+        'min': 0,
+        'function': self.op_stackswap
+      },
+      '[': {
+        'name': 'loop begin',
+        'min': 1,
+        'function': self.op_loopbegin
+      },
+      ']': {
+        'name': 'loop end',
+        'min': 0,
+        'function': self.op_loopend
+      },
+      '#': {
+        'name': 'comment',
+        'min': 0,
+        'function': self.op_comment
+      }
+    }
+  
+  def op_stringliteral(self, p):
+    ''''''
+    d = 1
+    while d < len(p) and p[d] != '"':
+      self.stack.append(ord(p[d]))
+      d += 1
+    return d
+  
+  def op_intliteral(self, p):
+    ''''''
+    d = 1
+    while d < len(p) and p[d].isdigit():
+      d += 1
+    self.stack.append(int(p[:d]))
+    return d - 1
+  
+  def op_comment(self, p):
+    d = 1
+    while d < len(p) and p[d] != '\n':
+      d += 1
+    self.op_newline(p)
+    return d
+
+  def op_newline(self, p):
+    ''''''
+    self.line_number += 1
+    self.char_number = 0
+
+  def op_whitespace(self, p):
+    ''''''
+    pass
+  
+  def op_loopbegin(self, p):
+    ''''''
+    if self.index not in self.loop.keys():
+      depth = 1
+      offset = 1
+      while offset < len(p) and depth > 0:
+        if p[offset] == ']':
+          depth -= 1
+        elif p[offset] == '[':
+          depth += 1
+        else:
+          offset += 1
+
+      self.loop[self.index] = self.index + offset
+      self.loop[offset + self.index] = self.index
+
+    if self.stack[-1] == 0:
+      self.index = self.loop[self.index]
+  
+  def op_loopend(self, p):
+    ''''''
+    self.index = self.loop[self.index] - 1
+
+  def op_debug(self, p):
+    ''''''
+    self.debug()
+  
+  def op_stackswap(self, p):
+    ''''''
+    t = self.stack
+    self.stack = self.ztack
+    self.ztack = t
+
+  def op_crosspop(self, p):
+    ''''''
+    self.ztack.append(self.stack.pop())
+
+  def op_print(self, p):
+    ''''''
+    print(chr(self.stack[-1]), end='')
+
+  def op_discard(self, p):
+    '''Pop the topmost item of the stack.'''
+    self.stack.pop()
+  
+  def op_duplicate(self, p):
+    '''Push a copy of the topmost item of the stack.'''
+    self.stack.append(self.stack[-1])
+  
+  def op_swap(self, p):
+    '''Swap the topmost two elements of the stack.'''
+    if len(p) > 1 and p[1] == '\\':
+      if len(self.stack) > 2:
+        self.op_3swap(p)
+        return 1
+      else:
+        self.error('not enough items on stack')
+    else:
+      s = self.stack.pop()
+      t = self.stack.pop()
+      self.stack.append(s)
+      self.stack.append(t)
+  
+  def op_3swap(self, p):
+    '''Swap the topmost and third-topmost elements of the stack.'''
+    s = self.stack.pop()
+    t = self.stack.pop()
+    u = self.stack.pop()
+    self.stack.append(s)
+    self.stack.append(t)
+    self.stack.append(u)
+  
+  def op_add(self, p):
+    '''Pop the two topmost elements and add them, pushing the result.'''
+    self.stack.append(self.stack.pop() + self.stack.pop())
+  
+  def op_subtract(self, p):
+    ''''''
+    self.stack.append(self.stack.pop() - self.stack.pop())
+  
+  def op_multiply(self, p):
+    ''''''
+    self.stack.append(self.stack.pop() * self.stack.pop())
+  
+  def op_divide(self, p):
+    ''''''
+    if len(p) > 1 and p[1] == '/': # integer division
+      if len(self.stack) > 1:
+        self.op_intdivide(p)
+        return 1
+      else:
+        self.error('not enough items on stack')
+    else:
+      if len(self.stack) > 1: # division
+        self.stack.append(self.stack.pop() / self.stack.pop())
+      else:
+        self.error('not enough items on stack')
+  
+  def op_intdivide(self, p):
+    ''''''
+    self.stack.append(self.stack.pop() // self.stack.pop())
+
+  def op_modulo(self, p):
+    ''''''
+    self.stack.append(self.stack.pop() % self.stack.pop())
+  
+  def op_power(self, p):
+    ''''''
+    self.stack.append(self.stack.pop() ** self.stack.pop())
   
   def reset(self):
     self.stack = []
@@ -31,9 +276,8 @@ class UcLang(object):
     self.store = {}
   
   def debug(self):
-    print('Debug Mode:')
     valid = ['c', 's', 'z']
-    prompt = '((c)ontinue, (s)tack, (z)tack) > '
+    prompt = 'Debug: ((c)ontinue, (s)tack, (z)tack) > '
     while True:
       c = input(prompt)
       if c == 'c':
@@ -73,148 +317,10 @@ class UcLang(object):
       p = program[self.index:]
       extra_advance = 0
 
-      if p[0] == ' ':
-        pass
-      elif p[0] == '\n':
-        self.line_number += 1
-        self.char_number = 0
-      elif p[0] == ';': # discard
-        if len(self.stack) > 0:
-          self.stack.pop()
-        else:
-          self.error('not enough items on stack')
-          break
-      elif p[0] == ':': # duplicate
-        if len(self.stack) > 0:
-          self.stack.append(self.stack[-1])
-        else:
-          self.error('not enough items on stack')
-          break
-      elif p[0] == '\\':
-        if len(self.stack) > 1:
-          if len(p) > 1 and p[1] == '\\': # rotate
-            if len(self.stack) > 2:
-              s = self.stack.pop()
-              t = self.stack.pop()
-              u = self.stack.pop()
-              self.stack.append(s)
-              self.stack.append(t)
-              self.stack.append(u)
-              extra_advance = 1
-            else:
-              self.error('not enough items on stack')
-              break
-          else: # swap
-            s = self.stack.pop()
-            t = self.stack.pop()
-            self.stack.append(s)
-            self.stack.append(t)
-        else:
-          self.error('not enough items on stack')
-          break
-      elif p[0] == '+': # add
-        if len(self.stack) > 1:
-          self.stack.append(self.stack.pop() + self.stack.pop())
-        else:
-          self.error('not enough items on stack')
-          break
-      elif p[0] == '-': # subtract
-        if len(self.stack) > 1:
-          self.stack.append(self.stack.pop() - self.stack.pop())
-        else:
-          self.error('not enough items on stack')
-          break
-      elif p[0] == '*': # multiply
-        if len(self.stack) > 1:
-          self.stack.append(self.stack.pop() * self.stack.pop())
-        else:
-          self.error('not enough items on stack')
-          break
-      elif p[0] == '/':
-        if len(p) > 1 and p[1] == '/': # integer division
-          if len(self.stack) > 1:
-            self.stack.append(self.stack.pop() // self.stack.pop())
-            extra_advance = 1
-          else:
-            self.error('not enough items on stack')
-        else:
-          if len(self.stack) > 1: # division
-            self.stack.append(self.stack.pop() / self.stack.pop())
-          else:
-            self.error('not enough items on stack')
-            break
-      elif p[0] == '%': # modulus
-        if len(self.stack) > 1:
-          self.stack.append(self.stack.pop() % self.stack.pop())
-        else:
-          self.error('not enough items on stack')
-          break
-      elif p[0] == '^': # power
-        if len(self.stack) > 1:
-          self.stack.append(self.stack.pop() ** self.stack.pop())
-        else:
-          self.error('not enough items on stack')
-          break
-        
-      elif p[0] == '[':
-        if self.index not in self.loop.keys():
-          depth = 1
-          offset = 1
-          while offset < len(p) and depth > 0:
-            if p[offset] == ']':
-              depth -= 1
-            elif p[offset] == '[':
-              depth += 1
-            else:
-              offset += 1
-
-          self.loop[self.index] = self.index + offset
-          self.loop[offset + self.index] = self.index
-
-        if len(self.stack) > 0:
-          if self.stack[-1] == 0:
-            self.index = self.loop[self.index]
-        else:
-          self.error('not enough items on stack')
-          break
-      elif p[0] == ']':
-        self.index = self.loop[self.index] - 1
-      elif p[0] == '.':
-        if len(self.stack) > 0:
-          print(chr(self.stack[-1]), end='')
-        else:
-          self.error('not enough items on stack')
-          break
+      if p[0] in self.commands.keys():
+        extra_advance = self.commands[p[0]]['function'](p)
       elif p[0].isdigit():
-        d = 1
-        while d < len(p) and p[d].isdigit():
-          d += 1
-        self.stack.append(int(p[:d]))
-        extra_advance = d - 1
-      elif p[0] == '#':
-        d = 1
-        while d < len(p) and p[d] != '\n':
-          d += 1
-        extra_advance += d
-      elif p[0] == '"':
-        # string literal
-        d = 1
-        while d < len(p) and p[d] != '"':
-          self.stack.append(ord(p[d]))
-          d += 1
-        extra_advance = d
-      elif p[0] == '$':
-        t = self.stack
-        self.stack = self.ztack
-        self.ztack = t
-      elif p[0] == '`':
-        if len(self.stack) > 0:
-          self.ztack.append(self.stack.pop())
-        else:
-          self.error('not enough items on stack')
-          break
-      elif p[0] == '_':
-        self.debug()
+        self.op_intliteral(p)
       else:
         self.error('unknown symbol \'{}\''.format(p[0]))
         break
